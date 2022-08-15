f936f1e7ff809e6967d050da2fbf91d691114bfb
Aman Dhyani <102688642+Aman-Dhyani@users.noreply.github.com>
2022-08-15 20:28:54 +0530

    Update spiderman.js

--- spiderman.js
+++ spiderman.js
@@ -15,7 +15,6 @@ let play = document.querySelector("#play");
 let btnLeft = document.querySelector("#btnLeft");
 let btnRight = document.querySelector("#btnRight");
 
-
 // ----------- music ------------------------------------
 let theme = new Audio("Music/theme.mp3");
 let gameover = new Audio("Music/gameover.m4a");
@@ -30,7 +29,6 @@ let carsound = new Audio("Music/car%20skid.m4a");
 let woosh = new Audio("Music/Knife,_cut_sound_effect(128k)%20Trim.m4a");
 let thanosaudio = new Audio("Music/thanos.m4a");
 
-
 // -------------- spiderMan controls -----------------------
 addEventListener("keydown", (e) => {
     if (e.key == "ArrowUp") jump();
@@ -43,18 +41,13 @@ function jump() {
     jumpSound.play();
     spiderweb.play();
 
-    setTimeout(() => {
-        spiderMan.classList.remove("jump");
-    }, 500);
+    setTimeout(() => spiderMan.classList.remove("jump"), 500);
 }
 
 // -------------- slide function ----------------------
 function slide() {
     spiderMan.classList.add("slide");
-
-    setTimeout(() => {
-        spiderMan.classList.remove("slide");
-    }, 500);
+    setTimeout(() => spiderMan.classList.remove("slide"), 500);
 }
 
 
@@ -75,21 +68,17 @@ function mainFunction() {
         clouds.classList.add("cloud");
         clouds.style.top = Math.floor(Math.random() * 200) + "px";
 
-        setTimeout(() => {
-            game.removeChild(clouds);
-        }, 4000);
+        setTimeout(() => game.removeChild(clouds), 4000);
     }, 1500);
 
     // ------------------- colison -------------------
-    requestAnimationFrame(collisions);
+    collisions();
 }
-
 
 // --------------------- Collisons -----------------------------------------------
 function collisions() {
 
     let coundown = 0;
-
     let count = setInterval(() => {
 
         coundown = coundown + 1
@@ -101,81 +90,67 @@ function collisions() {
         }
 
         // ------ gobyHide ---------------------
-        if (coundown > 14) {
-            goblin.classList.add("hide")
-        }
+        if (coundown > 14) goblin.classList.add("hide");
 
         // ---------- docokk showsup -------------
         if (coundown > 16) {
-            docock.classList.remove("hide")
-            car.classList.remove("hide")
+            docock.classList.remove("hide");
+            car.classList.remove("hide");
         }
 
         // ---------- gobyAudio pause -------------
-        if (coundown > 18) {
-            gobyTheme.pause()
-        }
+        if (coundown > 18) gobyTheme.pause();
 
         // ---------- electro showsup -------------
-        if (coundown > 25) {
-            electro.classList.remove("hide")
-        }
+        if (coundown > 25) electro.classList.remove("hide");
 
         // ---------- electro audio play -------------
-        if (coundown > 28) {
-            electroaudio.play()
-        }
+        if (coundown > 28) electroaudio.play();
 
         // ---------- docokkAudio pause -------------
-        if (coundown > 49) {
-            docaudio.pause()
-        }
+        if (coundown > 49) docaudio.pause();
 
         // ---------- docokk goingBack -------------
         if (coundown > 51) {
-            docock.classList.add("hide")
-            dummy2.classList.remove("hide")
+            docock.classList.add("hide");
+            dummy2.classList.remove("hide");
         }
 
         if (coundown > 58) {
-            dummy2.classList.add("hide")
-            electro.classList.add("Hide")
-            electroaudio.pause()
+            dummy2.classList.add("hide");
+            electro.classList.add("Hide");
+            electroaudio.pause();
         }
 
         // ---------- thanos showsup -------------
-        if (coundown > 64) {
-            spaceship.classList.remove("hide")
-        }
+        if (coundown > 64) spaceship.classList.remove("hide");
 
         if (coundown > 65) {
-            portal.classList.remove("hide")
-            sword.classList.remove("hide")
-            thanos.classList.remove("hide")
-            thanosaudio.play()
+            portal.classList.remove("hide");
+            sword.classList.remove("hide");
+            thanos.classList.remove("hide");
+            thanosaudio.play();
         }
 
         if (coundown > 72) {
-            goblin.classList.remove("hide")
-            goblin.classList.add("run")
-            gobyTheme.play()
+            goblin.classList.remove("hide");
+            goblin.classList.add("run");
+            gobyTheme.play();
         }
 
         if (coundown > 96) {
             thanosaudio.pause
-            portal.classList.add("hide")
-            sword.classList.add("hide")
-            thanos.classList.add("hide")
-            goblin.classList.add("hide")
-            gobyTheme.pause()
+            portal.classList.add("hide");
+            sword.classList.add("hide");
+            thanos.classList.add("hide");
+            goblin.classList.add("hide");
+            gobyTheme.pause();
         }
 
-        if (coundown > 98) {
-            spaceship.classList.add("spaceRev")
-        }
-
+        if (coundown > 98) spaceship.classList.add("spaceRev");
         if (coundown > 100) {
-            alert("you win")
+            alert("you win");
+            location.reload();
         }
 
         console.log(coundown);
@@ -183,16 +158,45 @@ function collisions() {
 
 
     // --------------------- collision detection for Goblin --------------------------------------
-    setInterval(() => {
-        gobyLeft = parseInt(getComputedStyle(goblin, null).getPropertyValue('left'));
-        gobytop = parseInt(getComputedStyle(goblin, null).getPropertyValue("top"))
-        spideyLeft = parseInt(getComputedStyle(spiderMan, null).getPropertyValue("left"))
-        spideytop = parseInt(getComputedStyle(spiderMan, null).getPropertyValue("top"))
+    leta = setInterval(() => {
 
+        // ------------ spidey property ---------------------------------
+        spideyLeft = parseInt(getComputedStyle(spiderMan, null).getPropertyValue("left"));
+        spideytop = parseInt(getComputedStyle(spiderMan, null).getPropertyValue("top"));
+        //console.log(spideytop);
+
+        // ------------ goby property -----------------------------------
+        gobyLeft = parseInt(getComputedStyle(goblin, null).getPropertyValue('left'));
+        gobytop = parseInt(getComputedStyle(goblin, null).getPropertyValue("top"));
+
+        // ------------ car property -----------------------------------
+        carLeft = parseInt(getComputedStyle(car, null).getPropertyValue('left'));
+        cartop = parseInt(getComputedStyle(car, null).getPropertyValue("top"));
+
+        // ------------ electro property --------------------------------
+        electroLeft = parseInt(getComputedStyle(electro, null).getPropertyValue('left'));
+        electrotop = parseInt(getComputedStyle(electro, null).getPropertyValue("top"));
+
+        // ------------ sword property ----------------------------------
+        swordsLeft = parseInt(getComputedStyle(sword, null).getPropertyValue('left'));
+        swordstop = parseInt(getComputedStyle(sword, null).getPropertyValue("top"));
+
+
+        // ------------ collision mechanics ------------------
         gobyspideyx = Math.abs(gobyLeft - spideyLeft)
         gobyspideyy = Math.abs(gobytop - spideytop)
-
         // console.log(gobyspideyx, gobyspideyy);
+
+        carspideyx = Math.abs(carLeft - spideyLeft)
+        carspideyy = Math.abs(cartop - spideytop)
+
+        electrospideyx = Math.abs(electroLeft - spideyLeft)
+        electrospideyy = Math.abs(electrotop - spideytop)
+
+        swordspideyx = Math.abs(swordsLeft - spideyLeft)
+        swordspideyy = Math.abs(swordstop - spideytop)
+
+        // --------------------- collision detection for goby & spidey ---------------------
         if (gobyspideyx < 50 && gobyspideyy < 80) {
             clearInterval(count, 100)
             gameover.play()
@@ -207,19 +211,9 @@ function collisions() {
                 window.location.reload();
             }, 1200);
         }
-    }, 10)
 
-    // --------------------- collision detection for Doc car --------------------------------------
-    setInterval(() => {
-        carLeft = parseInt(getComputedStyle(car, null).getPropertyValue('left'));
-        cartop = parseInt(getComputedStyle(car, null).getPropertyValue("top"))
-        spideyLeft = parseInt(getComputedStyle(spiderMan, null).getPropertyValue("left"))
-        spideytop = parseInt(getComputedStyle(spiderMan, null).getPropertyValue("top"))
-
-        carspideyx = Math.abs(carLeft - spideyLeft)
-        carspideyy = Math.abs(cartop - spideytop)
-
-        if (carspideyx < 100 && carspideyy < 120) {
+        // --------------------- collision detection for Doc car & spidey ---------------------------
+        if (carspideyx < 100 && spideytop < 500) {
             clearInterval(count, 100)
             gameover.play()
             carsound.play()
@@ -229,23 +223,13 @@ function collisions() {
                 gameover.currentTime = 0;
                 carsound.currentTime = 0;
                 gameover.pause();
-                carsound.pause()
+                carsound.pause();
                 window.location.reload();
             }, 1200);
         }
-    }, 10)
 
-    // --------------------- collision detection for Electro --------------------------------------
-    setInterval(() => {
-        electroLeft = parseInt(getComputedStyle(electro, null).getPropertyValue('left'));
-        electrotop = parseInt(getComputedStyle(electro, null).getPropertyValue("top"))
-        spideyLeft = parseInt(getComputedStyle(spiderMan, null).getPropertyValue("left"))
-        spideytop = parseInt(getComputedStyle(spiderMan, null).getPropertyValue("top"))
-
-        electrospideyx = Math.abs(electroLeft - spideyLeft)
-        electrospideyy = Math.abs(electrotop - spideytop)
-
-        if (electrospideyx < 100 && electrospideyy < 180) {
+        // --------------------- collision detection for Electro & spidey ------------------------------
+        if (electrospideyx < 60 && electrospideyy < 90) {
             clearInterval(count, 100)
             gameover.play()
             ezzz.play()
@@ -259,18 +243,7 @@ function collisions() {
                 window.location.reload();
             }, 1200);
         }
-    }, 10)
-
-    // --------------------- collision detection for thanosSword --------------------------------------
-    setInterval(() => {
-        swordsLeft = parseInt(getComputedStyle(sword, null).getPropertyValue('left'));
-        swordstop = parseInt(getComputedStyle(sword, null).getPropertyValue("top"))
-        spideyLeft = parseInt(getComputedStyle(spiderMan, null).getPropertyValue("left"))
-        spideytop = parseInt(getComputedStyle(spiderMan, null).getPropertyValue("top"))
-
-        swordspideyx = Math.abs(swordsLeft - spideyLeft)
-        swordspideyy = Math.abs(swordstop - spideytop)
-
+        // --------------------- collision detection for thanosSword & spidey ----------------------------
         if (swordspideyx < 100 && swordspideyy < 150) {
             clearInterval(count, 100)
             gameover.play()
@@ -290,7 +263,6 @@ function collisions() {
         }
     }, 10)
 }
-
 
 //----- RESET --------------------------------------------
 function reset() {
